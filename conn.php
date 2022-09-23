ad9806a1776f0f838a4095c0f2369b3a3a3f0917
Tazana2 <danisanmez@gmail.com>
2022-09-23 18:23:51 -0500

    Ya hay conexión remota con la DB :D

--- conn.php
+++ conn.php
@@ -1,16 +1,15 @@
-
-
 <?php  
-	$contrasena = '';
-	$usuario = 'root';
-	$nombrebd= 'tukicitas';
+	$host = 'bjqb8wn0h3bixmwd1gw7-mysql.services.clever-cloud.com';
+	$pwd = 'NwSPfaslKDZ0xp2x9tOC';
+	$user = 'uivehdn66sxhsaxq';
+	$dbName= 'bjqb8wn0h3bixmwd1gw7';
 
 	try {
 		$bd = new PDO(
-			'mysql:host=localhost;
-			dbname='.$nombrebd,
-			$usuario,
-			$contrasena,
+			'mysql:host=' . $host . ';
+			dbname='.$dbName,
+			$user,
+			$pwd,
 			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 		);
 	} catch (Exception $e) {
